387913ca82fe67d62d51b3f096efd923c0a945d6
Jerome <jeromeadrianfrancisco@gmail.com>
2026-03-09 12:11:00 -0600

    ...

--- PythonCode/EnrollSystem.py
+++ PythonCode/EnrollSystem.py
@@ -1,7 +1,40 @@
 
-def main():
-    print("something")
 
+
+# main method
+process = True
+
+def main():
+    while process:
+        SelectOption()
+    
+# methods & helpers
+def SelectOption():
+    # displays options for user to select
+    print("Welcome to the Student Enrollment System\n{}\nPlease select an option:\n{}\n{}\n{}\n{}".format(
+            hr(),"1. Enroll a new student", "2. Edit student enrollment", "3. Exit the application", hr()))
+    
+    # accepts an input and validates it
+    option = ValidateInputBasedOnCondition("Select your option: ", lambda x: x in ["1", "2", "3"])
+    
+    
+# this method takes in a condition and validate it BASED on custom conditions
+# this should make it such that the method can be used for various use cases in the CLI
+def ValidateInputBasedOnCondition(inputString, func):
+    option = input(inputString)
+
+    valid = func(option)
+
+    if valid:
+        return option
+    else:
+        print("Invalid input, please try again.")
+        return ValidateInputBasedOnCondition(inputString, func)
+
+# create a horizontal line for better UI
+# if a length arg is not provided, assume 50 characters
+def hr(length=50):
+    return "-" * length
 
 if __name__ == "__main__":
     main()
